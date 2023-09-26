b9e4b05d15da1c79d7e955f3fd2db566bdfc41b8
Reza Amiri <reza.amiri.ac@gmail.com>
2023-09-26 14:15:44 +0300

    add lesson 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import HomePage from './homepage.component';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return <div className='App'>Hello World</div>;
-  }
+function App() {
+  return (
+    <div className="App">
+      <HomePage/>
+    </div>
+  );
 }
 
 export default App;
+
